09d8948e60acf0fd12115c012285f8b9d0fefb4e
amryoussef92 <amryusif@gmail.com>
2025-04-18 14:05:39 +0400

    Add View component

--- src/Components/Tableshow.jsx
+++ src/Components/Tableshow.jsx
@@ -1,35 +1,42 @@
 import { useEffect, useState } from "react";
 import { db } from "./firebase";
-import { collection, getDocs } from "firebase/firestore";      
+import { collection, getDocs } from "firebase/firestore";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { Table } from "react-bootstrap";
+import View from "./View";
 function Tableshow(props) {
   const [users, setUsers] = useState([]);
+  const [selectedEmployee, setSelectedEmployee] = useState(null);
 
   useEffect(() => {
     const fetchEmployees = async () => {
       const querySnapshot = await getDocs(collection(db, "Employees"));
       const data = querySnapshot.docs.map((doc) => ({
         id: doc.id,
-        ...doc.data()
+        ...doc.data(),
       }));
 
-      setUsers(data); 
+      setUsers(data);
     };
 
     fetchEmployees();
   }, []);
 
   const showData = users.map((item, index) => (
-    <tr key={item.id}>
+    <tr
+      key={item.id}
+      onClick={() => setSelectedEmployee(item)} // Click the row to select employee
+      style={{ cursor: "pointer" }} // Add a pointer cursor to indicate that the row is clickable
+    >
       <td>{index + 1}</td>
       <td>{item.firstName}</td>
       <td>{item.lastName}</td>
       <td>{item.Email}</td>
-      <td>{item.Date}</td>
       <td>{item.salary}</td>
+      <td>{item.Date}</td>
+
       <td>
         <div className="d-flex align-items-center gap-2">
           <Link to={`${item.id}`}>
@@ -48,22 +55,28 @@ function Tableshow(props) {
   ));
 
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <td>ID</td>
-          <td>First Name</td>
-          <td>Last Name</td>
-          <td>Email</td>
-          <td>Salary</td>
-          <td>Date</td>
-          <td>Actions</td>
-        </tr>
-      </thead>
-      <tbody>
-        {showData}
-      </tbody>
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Email</td>
+            <td>Salary</td>
+            <td>Date</td>
+            <td>Actions</td>
+          </tr>
+        </thead>
+        <tbody>{showData}</tbody>
       </Table>
+      {selectedEmployee && (
+        <View
+          employee={selectedEmployee}
+          onClose={() => setSelectedEmployee(null)} // Close modal
+        />
+      )}
+    </>
   );
 }
 
